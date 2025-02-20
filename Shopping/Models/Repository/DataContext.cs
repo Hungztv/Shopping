04bf809a6144430386b666a8bbe36624b2b569a2
hungztv <22010106@st.phenikaa-uni.edu.vn>
2025-02-20 20:24:06 +0700

    20/2

--- Shopping/Models/Repository/DataContext.cs
+++ Shopping/Models/Repository/DataContext.cs
@@ -1,6 +1,16 @@
-﻿namespace Shopping.Models.Repository
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Shopping.Models.Repository
 {
-    public class DataContext : Dbcontext
+    public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<BrandModel> Brands { get; set; }
+        public DbSet<CategoryModel> Categories { get; set; }
+        public DbSet<ProductModel> Products { get; set; }
     }
 }
+
+
