bece479cf7494a417660b43fbdd077fc2cfbf47e
hieu47 <hieu472004a@gmail.com>
2025-03-10 02:41:44 +0700

    Trang danh mục sản phẩm

---
+++ Shopping/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shopping.Models.Repository;
+namespace Shopping.Areas.Admin.Controllers
+{
+  
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly DataContext _dataContext;
+        public CategoryController(DataContext context) 
+        {
+            _dataContext = context;
+            
+        }
+        public async Task<IActionResult> Index()
+        {
+
+
+            return View(await _dataContext.Categories
+                .OrderByDescending(p => p.Id) // Đúng cú pháp
+                .ToListAsync()); // Đúng chữ 'A' viết hoa);
+        }
+    }
+}
