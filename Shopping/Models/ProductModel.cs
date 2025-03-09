215439088b5f1d2f125453bd08312e88d594c997
hieu47 <hieu472004a@gmail.com>
2025-03-09 22:53:56 +0700

    them san pham

--- Shopping/Models/ProductModel.cs
+++ Shopping/Models/ProductModel.cs
@@ -1,15 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Shopping.Models.Repository.Validation;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Shopping.Models
 {
+
     public class ProductModel
     {
         [Key]
         public int Id { get; set; }
-        [Required,MinLength(4,ErrorMessage ="Yêu Cầu Nhập Tên Sản phẩm ")]
+        [Required, MinLength(4, ErrorMessage = "Yêu Cầu Nhập Tên Sản phẩm ")]
         public string Name { get; set; }
-        
+
         public string Slug { get; set; }
         [Required, MinLength(4, ErrorMessage = "Yêu Cầu Nhập Mô tả Sản phẩm ")]
         public string Description { get; set; }
@@ -17,7 +19,7 @@ namespace Shopping.Models
         public int BrandId { get; set; }
         public int CategoryId { get; set; }
         public CategoryModel Category { get; set; }
-        public BrandModel Brand { get; set; }   
+        public BrandModel Brand { get; set; }
         public String Image { get; set; }
         [NotMapped]
         [FileExtension]
