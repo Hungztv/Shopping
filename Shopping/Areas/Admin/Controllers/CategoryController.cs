690b540827319b2a6efb83d39b27937fa14ad477
hieu47 <hieu472004a@gmail.com>
2025-03-10 03:12:53 +0700

    trang danh mục sản phẩm

--- Shopping/Areas/Admin/Controllers/CategoryController.cs
+++ Shopping/Areas/Admin/Controllers/CategoryController.cs
@@ -1,6 +1,9 @@
 ﻿
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Shopping.Models;
 using Shopping.Models.Repository;
 namespace Shopping.Areas.Admin.Controllers
 {
@@ -15,12 +18,54 @@ namespace Shopping.Areas.Admin.Controllers
             
         }
         public async Task<IActionResult> Index()
+        { 
+
+
+            return View(await _dataContext.Categories.OrderByDescending(p => p.Id).ToListAsync()); 
+        }
+        public IActionResult Create()
+        {         
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CategoryModel category)
         {
+           
+            if (ModelState.IsValid)
+            {
+                //code them du lieu
+                category.Slug = category.Name.Replace(" ", "-");
+                var slug = await _dataContext.Categories.FirstOrDefaultAsync(p => p.Slug == category.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "Danh mục đã có trong database");
+                    return View(category);
+                }
+              
+                _dataContext.Add(category);
+                await _dataContext.SaveChangesAsync();
+                TempData["success"] = "Thêm danh mục thành công";
+                return RedirectToAction("Index");
 
+            }
+            else
+            {
+                TempData["error"] = "Có lỗi xảy ra";
+                List<string> errors = new List<string>();
+                foreach (var value in ModelState.Values)
 
-            return View(await _dataContext.Categories
-                .OrderByDescending(p => p.Id) // Đúng cú pháp
-                .ToListAsync()); // Đúng chữ 'A' viết hoa);
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMessages = string.Join("\n", errors);
+                return BadRequest(errorMessages);
+            }
+
+            return View(category);
         }
     }
 }
--- Shopping/Models/CategoryModel.cs
+++ Shopping/Models/CategoryModel.cs
@@ -6,9 +6,9 @@ namespace Shopping.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required,MinLength(4,ErrorMessage ="Yêu Cầu Nhập Tên Danh Mục")]
+        [Required(ErrorMessage ="Yêu Cầu Nhập Tên Danh Mục")]
         public string Name { get; set; }
-        [Required, MinLength(4, ErrorMessage = "Yêu Cầu Nhập Mô tả  Danh Mục")]
+        [Required(ErrorMessage = "Yêu Cầu Nhập Mô tả  Danh Mục")]
         public string Description { get; set; }
         
         public string Slug { get; set; }
