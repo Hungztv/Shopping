0d2f74fc74e1e08b51ee341a887a6631989c0ac4
hieu47 <hieu472004a@gmail.com>
2025-03-09 22:34:41 +0700

    them san pham

--- Shopping/Areas/Admin/Controllers/ProductController.cs
+++ Shopping/Areas/Admin/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
@@ -12,9 +13,11 @@ namespace Shopping.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly DataContext _dataContext;
-        public ProductController(DataContext context)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(DataContext context, IWebHostEnvironment webHostEnvironment)
         {
             _dataContext = context;
+            _webHostEnvironment = webHostEnvironment;
         }
         public async Task<IActionResult> Index()
         {
@@ -34,13 +37,39 @@ namespace Shopping.Areas.Admin.Controllers
             return View();
         }
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(ProductModel product)
         {
             ViewBag.Categories = new SelectList(_dataContext.Categories, "Id", "Name",product.CategoryId);
             ViewBag.Brands = new SelectList(_dataContext.Brands, "Id", "Name",product.BrandId);
             if (ModelState.IsValid)
             {
-                TempData["success"] = "ok heets";
+                //code them du lieu
+                product.Slug = product.Name.Replace(" ", "-");
+                var slug = await _dataContext.Products.FirstOrDefaultAsync(p => p.Slug == product.Slug);
+                if(slug != null)
+                {
+                    ModelState.AddModelError("", "Sản phẩm đã có trong database");
+                    return View(product);
+                }
+               
+                if(product.ImageUpload != null)
+                    {
+                        string uploadsDir = Path.Combine(_webHostEnvironment.WebRootPath,"media/products");
+                        string imageName = Guid.NewGuid().ToString() + "_" + product.ImageUpload.FileName;
+                        string filePath = Path.Combine(uploadsDir, imageName);
+
+                        FileStream fs = new FileStream(filePath, FileMode.Create);
+                        await product.ImageUpload.CopyToAsync(fs);
+                        fs.Close();
+                        product.Image = imageName;
+                    }
+                
+                _dataContext.Add(product);
+                await _dataContext.SaveChangesAsync();
+                TempData["success"] = "Thêm sản phẩm thành công";
+                return RedirectToAction("Index");
+
             }
             else
             {
