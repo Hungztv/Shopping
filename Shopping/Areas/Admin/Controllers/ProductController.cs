9ec914a46367ca554dc5143f4620ae8304e8321f
hungztv <22010106@st.phenikaa-uni.edu.vn>
2025-03-09 14:27:34 +0700

    tao admin

---
+++ Shopping/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.Models.Repository;
+
+namespace Shopping.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly DataContext _datacontext;
+        public ProductController(DataContext context)
+        {
+            _datacontext = context;
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
