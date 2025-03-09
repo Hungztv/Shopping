1c607299db3e01b947e631710aa0a625d532902a
hieu47 <hieu472004a@gmail.com>
2025-03-10 03:40:06 +0700

    Cập nhật danh mục sản phẩm

--- Shopping/Areas/Admin/Controllers/CategoryController.cs
+++ Shopping/Areas/Admin/Controllers/CategoryController.cs
@@ -23,6 +23,11 @@ namespace Shopping.Areas.Admin.Controllers
 
             return View(await _dataContext.Categories.OrderByDescending(p => p.Id).ToListAsync()); 
         }
+        public async Task<IActionResult> Edit(int Id)
+        {
+            CategoryModel category = await _dataContext.Categories.FindAsync(Id);
+            return View(category);
+        }
         public IActionResult Create()
         {         
             return View();
@@ -66,6 +71,56 @@ namespace Shopping.Areas.Admin.Controllers
             }
 
             return View(category);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(CategoryModel category)
+        {
+
+            if (ModelState.IsValid)
+            {
+                //code them du lieu
+                category.Slug = category.Name.Replace(" ", "-");
+                var slug = await _dataContext.Categories.FirstOrDefaultAsync(p => p.Slug == category.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "Danh mục đã có trong database");
+                    return View(category);
+                }
+
+                _dataContext.Update(category);
+                await _dataContext.SaveChangesAsync();
+                TempData["success"] = "Cập nhật danh mục thành công";
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                TempData["error"] = "Có lỗi xảy ra";
+                List<string> errors = new List<string>();
+                foreach (var value in ModelState.Values)
+
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMessages = string.Join("\n", errors);
+                return BadRequest(errorMessages);
+            }
+
+            return View(category);
+        }
+
+        public async Task<IActionResult> Delete(int Id)
+        {
+            CategoryModel category = await _dataContext.Categories.FindAsync(Id);
+           
+            _dataContext.Categories.Remove(category);
+            await _dataContext.SaveChangesAsync();
+            TempData["success"] = "Danh mục đã xóa";
+            return RedirectToAction("Index");
         }
     }
 }
