72faacce8e6328c6c1b903165b68f6845c098628
hieu47 <hieu472004a@gmail.com>
2025-03-14 01:03:04 +0700

    trang 404 not foung

--- Shopping/Controllers/HomeController.cs
+++ Shopping/Controllers/HomeController.cs
@@ -4,33 +4,44 @@ using Microsoft.EntityFrameworkCore;
 using Shopping.Models;
 using Shopping.Models.Repository;
 
-namespace Shopping.Controllers;
-
-public class HomeController : Controller
+namespace Shopping.Controllers
 {
-    private readonly DataContext _Datacontext;
-    private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger , DataContext context)
+    public class HomeController : Controller
     {
-        _logger = logger;
-        _Datacontext = context;
-    }
+        private readonly DataContext _Datacontext;
+        private readonly ILogger<HomeController> _logger;
 
-    public IActionResult Index()
-    {
-        var products = _Datacontext.Products.Include("Category").Include("Brand").ToList();
-        return View(products);
-    }
+        public HomeController(ILogger<HomeController> logger, DataContext context)
+        {
+            _logger = logger;
+            _Datacontext = context;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            var products = _Datacontext.Products.Include("Category").Include("Brand").ToList();
+            return View(products);
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error(int statuscode)
+        {
+            if(statuscode == 404)
+            {
+                return View("NotFound");
+
+            }
+            else
+            {
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+           
+        }
     }
 }
