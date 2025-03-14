fc7189f47d1423c2ea6060822e5d7a213d4b0c48
hungztv <22010106@st.phenikaa-uni.edu.vn>
2025-03-15 00:51:08 +0700

    tao checkout

--- Shopping/Models/Repository/DataContext.cs
+++ Shopping/Models/Repository/DataContext.cs
@@ -11,6 +11,8 @@ namespace Shopping.Models.Repository
         public DbSet<BrandModel> Brands { get; set; }
         public DbSet<CategoryModel> Categories { get; set; }
         public DbSet<ProductModel> Products { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+        public DbSet<OrderModel> Orders { get; set; }
     }
 }
 
