820525ddcc468e994bd88ccf795fe80724a3931b
hungztv <22010106@st.phenikaa-uni.edu.vn>
2025-03-15 16:05:17 +0700

    orderdetaisl

--- Shopping/Models/OrderDetails.cs
+++ Shopping/Models/OrderDetails.cs
@@ -8,6 +8,6 @@
         public long ProductId { get; set; }  
         public decimal Price { get; set; }
         public int Quantity { get; set; }
-
+        public ProductModel Product { get; set; }
     }
 }
