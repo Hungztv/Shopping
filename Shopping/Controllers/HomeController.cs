30918a1021de42403841b64094c0aab660f48c38
hungztv <22010106@st.phenikaa-uni.edu.vn>
2025-03-19 01:22:14 +0700

    update wishlist va compare

--- Shopping/Controllers/HomeController.cs
+++ Shopping/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Shopping.Models;
@@ -11,14 +12,18 @@ namespace Shopping.Controllers
     {
         private readonly DataContext _datacontext;
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<AppUserModel> _userManager;
 
-        public HomeController(ILogger<HomeController> logger, DataContext context)
+        public HomeController(ILogger<HomeController> logger, DataContext context, UserManager<AppUserModel> userManager)
         {
             _logger = logger;
             _datacontext = context;
+            _userManager = userManager;
+
         }
 
-       public IActionResult Index()
+
+        public IActionResult Index()
         {
             var products = _datacontext.Products.Include("Category").Include("Brand").ToList();
             var sliders = _datacontext.Sliders.Where(s => s.Status == 1).ToList();
@@ -36,6 +41,92 @@ namespace Shopping.Controllers
             return View();
         }
 
+        public async Task<IActionResult> Compare()
+        {
+            var compare_product = await (from c in _datacontext.Compares
+                                         join p in _datacontext.Products on c.ProductId equals p.Id
+                                         join u in _datacontext.Users on c.UserId equals u.Id
+                                         select new { User = u, Product = p, Compares = c })
+                               .ToListAsync();
+
+            return View(compare_product);
+        }
+        public async Task<IActionResult> DeleteCompare(int Id)
+        {
+            CompareModel compare = await _datacontext.Compares.FindAsync(Id);
+
+            _datacontext.Compares.Remove(compare);
+
+            await _datacontext.SaveChangesAsync();
+            TempData["success"] = "So sánh đã được xóa thành công";
+            return RedirectToAction("Compare", "Home");
+        }
+        public async Task<IActionResult> DeleteWishlist(int Id)
+        {
+            WishlistModel wishlist = await _datacontext.Wishlists.FindAsync(Id);
+
+            _datacontext.Wishlists.Remove(wishlist);
+
+            await _datacontext.SaveChangesAsync();
+            TempData["success"] = "Yêu thích đã được xóa thành công";
+            return RedirectToAction("Wishlist", "Home");
+        }
+        public async Task<IActionResult> Wishlist()
+        {
+            var wishlist_product = await (from w in _datacontext.Wishlists
+                                          join p in _datacontext.Products on w.ProductId equals p.Id
+                                          select new { Product = p, Wishlists = w })
+                               .ToListAsync();
+
+            return View(wishlist_product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddWishlist(int Id, WishlistModel wishlistmodel)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            var wishlistProduct = new WishlistModel
+            {
+                ProductId = Id,
+                UserId = user.Id
+            };
+
+            _datacontext.Wishlists.Add(wishlistProduct);
+            try
+            {
+                await _datacontext.SaveChangesAsync();
+                return Ok(new { success = true, message = "Add to wishlisht Successfully" });
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "An error occurred while adding to wishlist table.");
+            }
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddCompare(int Id)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            var compareProduct = new CompareModel
+            {
+                ProductId = Id,
+                UserId = user.Id
+            };
+
+            _datacontext.Compares.Add(compareProduct);
+            try
+            {
+                await _datacontext.SaveChangesAsync();
+                return Ok(new { success = true, message = "Add to compare Successfully" });
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "An error occurred while adding to compare table.");
+            }
+
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error(int statuscode)
