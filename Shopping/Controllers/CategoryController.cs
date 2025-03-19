4716e0f927555af344112a234208a932ddbde2d2
hungztv <22010106@st.phenikaa-uni.edu.vn>
2025-03-19 21:22:55 +0700

    sap xep san pham

--- Shopping/Controllers/CategoryController.cs
+++ Shopping/Controllers/CategoryController.cs
@@ -1,26 +1,85 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Shopping.Models;
 using Shopping.Models.Repository;
+using Shopping.Models;
 
-namespace Shopping.Controllers
+
+namespace Shopping_Tutorial.Controllers
 {
     public class CategoryController : Controller
     {
-        private readonly DataContext _datacontext;
+        private readonly DataContext _dataContext;
         public CategoryController(DataContext context)
         {
-            _datacontext = context;
+            _dataContext = context;
         }
-        public async  Task<IActionResult> Index(string Slug="")
+        public async Task<IActionResult> Index(string slug = "", string sort_by = "", string startprice = "", string endprice = "")
         {
-            CategoryModel category = _datacontext.Categories.Where(x => x.Slug == Slug).FirstOrDefault();
+            CategoryModel category = _dataContext.Categories.Where(c => c.Slug == slug).FirstOrDefault();
+
+
             if (category == null)
             {
                 return RedirectToAction("Index");
             }
-            var productsByCategory = _datacontext.Products.Where(p => p.Category.Id == category.Id);
-            return View(await productsByCategory.OrderByDescending(p => p.Id).ToListAsync());
+
+            ViewBag.Slug = slug;
+            IQueryable<ProductModel> productsByCategory = _dataContext.Products.Where(p => p.CategoryId == category.Id);
+            var count = await productsByCategory.CountAsync();
+            if (count > 0)
+            {
+                // Apply sorting based on sort_by parameter
+                if (sort_by == "price_increase")
+                {
+                    productsByCategory = productsByCategory.OrderBy(p => p.Price);
+                }
+                else if (sort_by == "price_decrease")
+                {
+                    productsByCategory = productsByCategory.OrderByDescending(p => p.Price);
+                }
+                else if (sort_by == "price_newest")
+                {
+                    productsByCategory = productsByCategory.OrderByDescending(p => p.Id);
+                }
+                else if (sort_by == "price_oldest")
+                {
+                    productsByCategory = productsByCategory.OrderBy(p => p.Id);
+                }
+                else if (startprice != "" && endprice != "")
+                {
+                    decimal startPriceValue;
+                    decimal endPriceValue;
+
+                    if (decimal.TryParse(startprice, out startPriceValue) && decimal.TryParse(endprice, out endPriceValue))
+                    {
+                        //productsByCategory = productsByCategory
+                            //.Where(p => decimal.TryParse(p.Price, out decimal price) && price >= startPriceValue && price <= endPriceValue);
+                    }
+                    else
+                    {
+                        productsByCategory = productsByCategory.OrderByDescending(p => p.Id);
+                    }
+
+                }
+                else
+                {
+                    productsByCategory = productsByCategory.OrderByDescending(p => p.Id);
+                }
+
+                //decimal minPrice = await productsByCategory.MinAsync(p => p.Price);
+                //decimal maxPrice = await productsByCategory.MaxAsync(p => p.Price);
+
+
+                ViewBag.sort_key = sort_by;
+
+                ViewBag.count = count;
+
+                //ViewBag.minprice = minPrice;
+                //ViewBag.maxprice = maxPrice;
+            }
+
+            // Add more sorting options if needed (e.g., name, date)
+            return View(await productsByCategory.ToListAsync());
         }
     }
 }
